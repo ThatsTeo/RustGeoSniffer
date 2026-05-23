79f054a22f4dc853d2dd37524eb2c2d85026bfab
ThatsTeo <matteo.carlotestini@gmail.com>
2026-05-23 17:13:04 +0200

    Tested some fn in main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 mod geolocation_api;
 mod rppal_gpio;
+mod tg_bot;
 mod wifi_scan;
 
-fn main() {
-    println!("Hello World!");
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("[DEBUG] Program test...");
     // let reti = wifi_scan::iwlist_output_parsed();
     // print!("{:?}", reti);
 
-    println!("{}", geolocation_api::get_api());
+    let (lat, long, acc) = geolocation_api::get_coords()?;
+    println!("{}, {}, {}", lat, long, acc);
+    println!("[DEBUG] Test ended!");
+    Ok(())
 }
