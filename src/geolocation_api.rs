19072b283f82616215aefd6f5d99bc24920a48a8
ThatsTeo <matteo.carlotestini@gmail.com>
2026-05-23 17:12:53 +0200

    Added fn to get coordinates with a POST to Google Cloud

--- src/geolocation_api.rs
+++ src/geolocation_api.rs
@@ -1,16 +1,18 @@
-use std::fs;
+use reqwest;
+use std::{fmt::format, fs};
+
+use crate::wifi_scan::iwlist_output_parsed;
 
 pub fn get_api() -> String {
     let contents =
         fs::read_to_string("config.txt").expect("[API DEBUG] File 'config.txt' not found!");
-    let mut gcg_api: &str = "";
 
     for line in contents.lines() {
         let line = line.trim();
 
-        if line.starts_with("GCGEOLOCATION_API=") {
+        if line.starts_with("GOOGLE_API_KEY=") {
             if let Some((_api, value)) = line.split_once('=') {
-                println!("[API DEBUG] Geolocation API found! Returning API...");
+                println!("[API DEBUG] Geolocation API found!");
                 return value.to_string();
             }
         }
@@ -18,4 +20,37 @@ pub fn get_api() -> String {
 
     println!("[API DEBUG] API not found! Check again file `config.txt`...");
     String::new()
+}
+
+pub fn get_coords() -> Result<(f32, f32, f32), Box<dyn std::error::Error>> {
+    let url = "https://www.googleapis.com/geolocation/v1/geolocate?key=";
+    let gcg_api = get_api();
+
+    // init payload
+    let mut payload = serde_json::json!({
+        "wifiAccessPoints": []
+    });
+
+    // parse JSON
+    let networks = iwlist_output_parsed();
+    for (mac, rssi) in &networks {
+        if let Some(array) = payload["wifiAccessPoints"].as_array_mut() {
+            array.push(serde_json::json!({
+                "macAddress": mac,
+                "signalStrength": rssi
+            }));
+        }
+    }
+
+    let res = reqwest::blocking::Client::new()
+        .post(format!("{url}{gcg_api}"))
+        .json(&payload)
+        .send()?
+        .json::<serde_json::Value>()?;
+
+    let lat = res["location"]["lat"].as_f64().unwrap_or(0.0) as f32;
+    let lng = res["location"]["lng"].as_f64().unwrap_or(0.0) as f32;
+    let accuracy = res["accuracy"].as_f64().unwrap_or(0.0) as f32;
+
+    Ok((lat, lng, accuracy))
 }
