28aeb610657a8e17de9ed13176593dd713d26757
ThatsTeo <matteo.carlotestini@gmail.com>
2026-05-25 12:42:06 +0200

    Created file to start project

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+API_KEY=$(grep "^TELEGRAM_TOKEN=" config.txt | cut -d'=' -f2 | tr -d '[:space:]')
+
+if [ -z "$API_KEY" ]; then
+    echo "[SH BOT TOKEN ERROR] Token not found"
+    exit 1
+fi
+
+echo "Bot TOKEN found!"
+export TELOXIDE_TOKEN=$API_KEY
+
+./target/release/rust_geo_sniffer
