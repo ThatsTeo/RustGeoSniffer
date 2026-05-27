3a2eed0077281e54c18fee1c7b843dc4945fab48
ThatsTeo <matteo.carlotestini@gmail.com>
2026-05-27 14:54:19 +0200

    Improved buzzer sound

--- src/rppal_gpio.rs
+++ src/rppal_gpio.rs
@@ -16,11 +16,11 @@ pub fn beep_buzzer() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 
     println!("[GPIO DEBUG] Buzzer ready on PIN {}", BUZZ_PIN);
 
-    for _ in 0..3 {
+    for _ in 0..4 {
         buzzer_output.set_high();
         thread::sleep(Duration::from_millis(200));
         buzzer_output.set_low();
-        thread::sleep(Duration::from_millis(200));
+        thread::sleep(Duration::from_millis(100));
     }
 
     println!("[GPIO DEBUG] Beep ended...");
