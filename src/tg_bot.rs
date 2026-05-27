54e0d18964459a18d38eaab5f94f1100d3432edb
ThatsTeo <matteo.carlotestini@gmail.com>
2026-05-27 11:55:19 +0200

    Added auto locate function

--- src/tg_bot.rs
+++ src/tg_bot.rs
@@ -1,7 +1,11 @@
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::time;
 use teloxide::{prelude::*, utils::command::BotCommands};
 
 use crate::geolocation_api::get_coords;
 use crate::rppal_gpio::{self};
+
+static STOP_THREAD: AtomicBool = AtomicBool::new(false);
 
 #[tokio::main]
 pub async fn run_bot() {
@@ -29,7 +33,7 @@ enum Command {
     #[command(description = "Beep the buzzer 3 times")]
     Beep,
     #[command(description = "Set a delay between scans (used for /startautolocate)")]
-    SetDelay,
+    SetDelay(u64),
     #[command(description = "Start automaticcaly locate the device")]
     StartAutoLocate,
     #[command(description = "Stop auto locate if active")]
@@ -37,6 +41,9 @@ enum Command {
 }
 
 async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
+    let delay: u64 = 1;
+    // let mut stopThread: bool = false;
+
     match cmd {
         Command::Help => {
             bot.send_message(msg.chat.id, Command::descriptions().to_string())
@@ -65,15 +72,45 @@ async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
             if let Err(e) = rppal_gpio::beep_buzzer() {
                 let err_msg = e.to_string();
                 println!("[BOT ERROR]: {}", err_msg);
-                bot.send_message(msg.chat.id, "Couldn't make the buzzer beep!")
+                let _ = bot
+                    .send_message(msg.chat.id, "Couldn't make the buzzer beep!")
                     .await;
             }
             bot.send_message(msg.chat.id, "Buzzer finished beeping.")
                 .await?
         }
-        Command::SetDelay => bot.send_message(msg.chat.id, "Delay").await?,
-        Command::StartAutoLocate => bot.send_message(msg.chat.id, "Start").await?,
-        Command::StopAutoLocate => bot.send_message(msg.chat.id, "Stop").await?,
+        Command::SetDelay(delay) => {
+            bot.send_message(msg.chat.id, format!("Delay setted to {delay} minute(s)!"))
+                .await?
+        }
+        Command::StartAutoLocate => {
+            if STOP_THREAD.load(Ordering::Relaxed) {
+                bot.send_message(msg.chat.id, format!("Auto location is already started!"))
+                    .await?
+            } else {
+                STOP_THREAD.store(false, Ordering::Relaxed);
+                let bot_clone = bot.clone();
+                let msg_clone = msg.clone();
+
+                tokio::spawn(async move {
+                    if let Err(e) = auto_locate(delay, bot_clone, msg_clone).await {
+                        println!("[AUTO LOCATE ERROR]: {e}");
+                    }
+                });
+                bot.send_message(msg.chat.id, "Auto locate started!")
+                    .await?
+            }
+        }
+        Command::StopAutoLocate => {
+            if !STOP_THREAD.load(Ordering::Relaxed) {
+                bot.send_message(msg.chat.id, format!("Auto location is alredy off!"))
+                    .await?
+            } else {
+                STOP_THREAD.store(true, Ordering::Relaxed);
+                bot.send_message(msg.chat.id, "Auto locating stopped!")
+                    .await?
+            }
+        }
     };
 
     Ok(())
@@ -88,4 +125,27 @@ fn locate_msg() -> Result<String, Box<dyn std::error::Error + Send + Sync>> {
         );
     }
     Ok(msg)
+}
+
+async fn auto_locate(
+    delay: u64,
+    bot: Bot,
+    msg: Message,
+) -> Result<String, Box<dyn std::error::Error + Send + Sync>> {
+    loop {
+        if STOP_THREAD.load(Ordering::Relaxed) {
+            STOP_THREAD.store(false, Ordering::Relaxed);
+            return Ok(String::from("Auto locate stopped."));
+        }
+        let response = tokio::task::spawn_blocking(|| match locate_msg() {
+            Ok(m) => m,
+            Err(e) => format!("[ERROR] {e}"),
+        })
+        .await
+        .unwrap_or_else(|e| format!("[ERROR] {e}"));
+
+        bot.send_message(msg.chat.id, response).await?;
+
+        tokio::time::sleep(time::Duration::from_mins(delay)).await;
+    }
 }
